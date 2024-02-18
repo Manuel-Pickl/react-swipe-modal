198f975d3ca99d873480c28655902f1ecc3a3697
Manuel Pickl <picklmanuel98@gmail.com>
2024-02-18 11:10:14 +0100

    npm init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sassPlugin from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sassPlugin(),],
+  build: {
+    lib: {
+      entry: 'src/components/SwipeModal/SwipeModal.tsx',
+      name: 'SwipeModal',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        assetFileNames: 'style.css',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
