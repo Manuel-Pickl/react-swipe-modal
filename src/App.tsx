99e495ddb83d7e2e427702be204dfb48f4d98b5c
Manuel Pickl <picklmanuel98@gmail.com>
2024-02-18 15:15:52 +0100

    removed old code

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,18 @@ import { useRef } from "react";
 import SwipeModal, { SwipeModalRef } from "./components/SwipeModal/SwipeModal";
 
 function App() {
-    const inProduction = import.meta.env.VITE_IN_PRODUCTION === "true";
     const swipeModalRef = useRef<SwipeModalRef>(null);
     
     return (
         <>
-            {inProduction ? (
-                <SwipeModal />
-            ) : (
-                <>
-                    <button onClick={() => swipeModalRef.current?.show()}
-                    >
-                        Open
-                    </button>
+            <button onClick={() => swipeModalRef.current?.show()}
+            >
+                Open
+            </button>
 
-                    <SwipeModal ref={swipeModalRef}>
-                        <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                    </SwipeModal>
-                </>
-            )}
+            <SwipeModal ref={swipeModalRef}>
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            </SwipeModal>
         </>
     );
 }
