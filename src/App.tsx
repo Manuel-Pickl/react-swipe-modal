9e1394f90c7f70e3d1ac9df3cd2122092226b4c0
Manuel Pickl <picklmanuel98@gmail.com>
2024-02-24 18:26:50 +0100

    demo page: basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,148 @@
 // App.tsx
-import { useRef } from "react";
+import "./App.scss";
+import { useEffect, useRef, useState } from "react";
 import { SwipeModalRef } from "./components/SwipeModal/SwipeModal.types";
 import SwipeModal from "./components/SwipeModal/SwipeModal";
 
 function App() {
     const swipeModalRef = useRef<SwipeModalRef>(null);
-    
-    return (
-        <>
-            <button 
-                onClick={() => swipeModalRef.current?.show()}
-                style={{
-                    position: "absolute",
-                    inset: 0,
-                    backgroundColor: "transparent",
-                }}
-            >
-                Click anywhere to open modal
-            </button>
 
-            <SwipeModal ref={swipeModalRef}>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            </SwipeModal>
-        </>
+    // styling
+    const [animationDuration, setAnimationDuration] = useState<number>(350);
+    const [backdropOpacity, setBackdropOpacity] = useState<number>(0.3);
+    const [barColor, setBarcolor] = useState<string>("#696969");
+    const [borderRadius, setBorderRadius] = useState<string>("1.3rem");
+    const [hideBar, setHideBar] = useState<boolean>(false);
+    const [maxWidth, setMaxWidth] = useState<string>("100vw");
+    const [modalColor, setModalColor] = useState<string>("#1a1a1a");
+    
+    // functionality
+    const [closeTrigger, setCloseTrigger] = useState<'swipe'|'height'>('swipe')
+    const [closeTriggerPercentage, setCloseTriggerPercentage] = useState<number>(50);
+    const [closeTriggerSpeed, setCloseTriggerSpeed] = useState<number>(500);
+    const [disableSwipe, setDisableSwipe] = useState<boolean>(false);
+    const [swipeOnlyFromBar, setSwipeOnlyFromBar] = useState<boolean>(false);
+
+    useEffect(() => {
+        const backdrop: HTMLElement | null = document.querySelector(".backdrop");
+        if (!backdrop) {
+            return;
+        }
+
+        backdrop.style.opacity = backdropOpacity.toString();
+    }, [backdropOpacity]);
+
+    return (
+        <div className="app">
+            <h1>Demo</h1>
+
+            <div className="options">
+                <h2>Styling</h2>
+
+                <label>animationDuration:</label>
+                <div className="input">
+                    <label>0</label>
+                    <input type="range" min={0} max={2000} value={animationDuration} onChange={(e) => setAnimationDuration(e.target.valueAsNumber)} />
+                    <label>2000</label>
+                </div>
+
+                <label>backdropOpacity:</label>
+                <div className="input">
+                    <label>0</label>
+                    <input type="range" min={0} max={100} value={backdropOpacity * 100} onChange={(e) => setBackdropOpacity(e.target.valueAsNumber / 100)} />
+                    <label>1</label>
+                </div>
+
+                <label>barColor:</label>
+                <div className="input">
+                    <input type="color" value={barColor} onChange={(e) => setBarcolor(e.target.value)} />
+                </div>
+
+                <label>borderRadius:</label>
+                <div className="input">
+                    <input type="text" value={borderRadius} onChange={(e) => setBorderRadius(e.target.value)} />
+                </div>
+
+                <label>hideBar:</label>
+                <div className="input">
+                    <input type="checkbox" checked={hideBar} onChange={(e) => setHideBar(e.target.checked)} />
+                </div>
+
+                <label>maxWidth:</label>
+                <div className="input">
+                    <input type="text" value={maxWidth} onChange={(e) => setMaxWidth(e.target.value)} />
+                </div>
+
+                <label>modalColor:</label>
+                <div className="input">
+                    <input type="color" value={modalColor} onChange={(e) => setModalColor(e.target.value)} />
+                </div>
+
+                <h2>Functionality</h2>
+
+                <label>closeTrigger:</label>
+                <div className="input">
+                    <select value={closeTrigger} onChange={(e) => setCloseTrigger(e.target.value as 'swipe' | 'height')}>
+                        <option value="swipe">Swipe</option>
+                        <option value="height">Height</option>
+                    </select>
+                </div>
+
+                <label>closeTriggerPercentage:</label>
+                <div className="input">
+                    <label>0</label>
+                    <input type="range" min={0} max={100} value={closeTriggerPercentage} onChange={(e) => setCloseTriggerPercentage(e.target.valueAsNumber)} />
+                    <label>100</label>
+                </div>
+
+                <label>closeTriggerSpeed:</label>
+                <div className="input">
+                    <label>0</label>
+                    <input type="range" min={0} max={2000} value={closeTriggerSpeed} onChange={(e) => setCloseTriggerSpeed(e.target.valueAsNumber)} />
+                    <label>2000</label>
+                </div>
+
+                <label>disableSwipe:</label>
+                <div className="input">
+                    <input type="checkbox" checked={disableSwipe} onChange={(e) => setDisableSwipe(e.target.checked)} />
+                </div>
+
+                <label>swipeOnlyFromBar:</label>
+                <div className="input">
+                    <input type="checkbox" checked={swipeOnlyFromBar} onChange={(e) => setSwipeOnlyFromBar(e.target.checked)} />
+                </div>
+            </div>
+
+            <div className="test-window">
+                <button onClick={() => swipeModalRef.current?.show()}>
+                    Open
+                </button>
+
+                <SwipeModal
+                    ref={swipeModalRef}
+
+                    // styling
+                    animationDuration={animationDuration}
+                    backdropOpacity={backdropOpacity}
+                    barColor={barColor}
+                    borderRadius={borderRadius}
+                    hideBar={hideBar}
+                    maxWidth={maxWidth}
+                    modalColor={modalColor}
+
+                    // functionality
+                    closeTrigger={closeTrigger}
+                    closeTriggerPercentage={closeTriggerPercentage}
+                    closeTriggerSpeed={closeTriggerSpeed}
+                    disableSwipe={disableSwipe}
+                    swipeOnlyFromBar={swipeOnlyFromBar}
+
+                    backdropStyle={{ position: "absolute", height: "100%", width: "100%"}}
+                >
+                    <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </SwipeModal>
+            </div>
+        </div>
     );
 }
 
